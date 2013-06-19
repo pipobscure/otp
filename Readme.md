2b476f0d74f74cd7bb6fd7b4d40e74c8c999a140
Philipp Dunkel <p.dunkel@durchblicker.at>
2013-06-19 21:31:50 +0200

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,42 @@
+# OTP
+
+This is a utility to work with Google-Authenticator and compatible OTP-Mechanisms.
+
+ * HOTP (HMAC-Based One-Time Password Algorithm): [RFC 4226](http://tools.ietf.org/html/rfc4226)
+ * TOTP (Time-Based One-Time Password Algorithm): [RFC 6238](http://tools.ietf.org/html/rfc6238)
+
+## Main Function
+
+    var otp = OTP(options);
+    otp.hotp(counter); // Generates an OTP using HTOP method
+    otp.totp(); // Generates an OTP using TOTP method
+    otp.secret; // Base32 encoded secret
+    otp.totpURL; // A TOTP-URL that can be used with Google-Authenticator
+    otp.HotpURL; // A HOTP-URL that can be used with Google-Authenticator
+
+Options can have the following properties:
+
+ * **name**: A name used in generating URLs
+ * **keySize**: The size of the OTP-Key (default 32)
+ * **codeLength**: The length of the code generated (default 6)
+ * **secret**: The secret (either a Buffer of Base32-encoded String)
+ * **epoch**: The seconds since Unix-Epoch to use as a base for calculating the TOTP (default 0)
+ * **timeSlice**: The timeslice to use for calculating counter from time in seconds (default 30)
+
+## OTP.parse(string)
+
+Parses an OTP-URL or Base32-Encoded Secret.
+
+## OTP.reviveJSON
+
+A JSON-reviver to revive stringified OTP objects
+
+## License (MIT)
+
+Copyright (C) 2013 Philipp Dunkel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ otp.js
@@ -0,0 +1,121 @@
+/*
+** © 2013 by Philipp Dunkel <p.dunkel@me.com>. Licensed unde MIT-License
+*/
+
+module.exports = OTP;
+
+var Crypto = require('crypto');
+var Base32 = require('thirty-two');
+
+function OTP(options) {
+  if ('string' === typeof options) return OTP.parse(options);
+  if (!(this instanceof OTP)) return new OTP(options);
+  options = clone(options || {});
+  options.name = String(options.name || 'OTP-Authentication').split(/[^\w|_|-|@]/).join('');
+  options.keySize =  isNaN(options.keySize) ? 32 : options.keySize;
+  options.codeLength = isNaN(options.codeLength) ? 6 : options.codeLength;
+  options.secret = options.secret || generateKey(options.keySize);
+  options.epoch = (isNaN(options.epoch) ? 0 : options.epoch) * 1000;
+  options.timeSlice = (isNaN(options.timeSlice) ? 30 : options.timeSlice) * 1000;
+  Object.keys(OTP.prototype).forEach(function(method) {
+    if ('function' !== typeof OTP.prototype[method]) return;
+    Object.defineProperty(this, method, {
+      value: OTP.prototype[method].bind(this, options)
+    });
+  }.bind(this));
+
+  Object.defineProperty(this, 'secret', {
+    value:options.secret,
+    enumerable:true
+  });
+  Object.defineProperty(this, 'totpURL', {
+    value:['otpauth://totp/', options.name, '?secret=', encodeURIComponent(this.secret) ].join(''),
+    enumerable:true
+  });
+  Object.defineProperty(this, 'hotpURL', {
+    value:['otpauth://hotp/', options.name, '?secret=', encodeURIComponent(this.secret) ].join(''),
+    enumerable:true
+  });
+}
+
+function generateKey(length) {
+  var set = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz!@#$%^&*()<>?/[]{},.:;';
+  var res = '';
+  while(res.length < length) {
+    res += set[Math.floor(Math.random() * set.length)];
+  }
+  return Base32.encode(res).replace(/=/g, '');;
+}
+
+OTP.parse = function(str, options) {
+  options = clone(options || {});
+  str = String(str || '');
+  var url = /^otpauth:\/\/[t|h]opt\/([\s|\S]+?)\?secret=([\s|\S]+)$/.exec(str);
+  if (url) {
+    options.name = url[1];
+    options.secret = url[2];
+  } else {
+    options.secret = str;
+  }
+  return new OTP(options);
+};
+
+OTP.prototype.hotp = function(options, counter) {
+  var hmac = Crypto.createHmac('sha1', new Buffer(Base32.decode(options.secret)));
+  hmac = new Buffer(hmac.update(UInt64Buffer(counter)).digest('hex'), 'hex');
+  var offset = hmac[19] & 0xf;
+  var code = String((hmac[offset] & 0x7f) << 24 | (hmac[offset + 1] & 0xff) << 16 | (hmac[offset + 2] & 0xff) << 8 | (hmac[offset + 3] & 0xff));
+  code = ((new Array(options.codeLength + 1)).join('0')+code).slice(-1 * options.codeLength);
+  return code;
+};
+
+function UInt64Buffer(num) {
+  var res = [];
+  while (res.length < 8) {
+    res.unshift(num & 0xFF);
+    num = num >> 8;
+  }
+  return new Buffer(res);
+}
+
+OTP.prototype.totp = function(options) {
+  var now = isNaN(options.now) ? Date.now() : options.now;
+  var counter = Math.floor((now - options.epoch) / options.timeSlice);
+  return this.hotp(counter);
+};
+OTP.prototype.toString = function() {
+  return '[object OTP]';
+};
+OTP.classID = 'OTP{@phidelta}';
+OTP.prototype.toJSON = function(options) {
+  var res = {
+    'class':OTP.classID,
+    name:options.name,
+    keySize:options.keySize,
+    codeLength:options.codeLength,
+    secret:this.secret,
+    epoch:options.epoch / 1000,
+    timeSlice:options.timeSlice / 1000
+  };
+  return res;
+};
+OTP.reviveJSON = function(key, val) {
+  if (val['class'] !== OTP.classID) return val;
+  return OTP(val);
+};
+
+function clone(obj) {
+  if ('object' !== typeof obj) {
+    return obj;
+  } else if (Array.isArray(obj)) {
+    return obj.map(clone);
+  } else if (obj instanceof Date) {
+    return new Date(obj.getTime());
+  }
+
+  var res = {};
+  Object.keys(obj).forEach(function(key) {
+    res[key] = clone(obj[key]);
+  });
+  return res;
+}
