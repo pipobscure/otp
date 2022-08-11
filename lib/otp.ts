fea4e49e57148b82b968b8b5544cd9f03993bbe3
Emanuele Tozzato <etozzato@gmail.com>
2022-08-10 23:10:15 -0700

    Removes zeroBuffer constant

--- lib/otp.ts
+++ lib/otp.ts
@@ -99,8 +99,6 @@ function UInt64Buffer(num: number) {
 	return res;
 }
 
-const zeroBuffer = new Uint8Array(new Array(128).fill(0));
-
 class Hmac {
 	constructor(blocksize: number, key: Uint8Array) {
 		if (blocksize !== 128 && blocksize !== 64) {
@@ -140,7 +138,7 @@ function rekey(key: Uint8Array, blocksize: number): Uint8Array {
 	if (key.length < blocksize) {
 		const res = new Uint8Array(blocksize);
 		res.set(key);
-		res.set(zeroBuffer, key.length);
+		res.fill(0, key.length);
 		return res;
 	}
 	return key;
